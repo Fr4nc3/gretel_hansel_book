b6fb249d187a331fc020fbf88fadec9ad5b2ca28
Francia Riesco <ff2214@columbia.edu>
2020-06-27 14:28:08 -0400

    angular cordova working version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,14 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { HttpModule } from "@angular/http";
-import { FormsModule } from "@angular/forms";
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
-import { MatIconModule } from "@angular/material/icon";
-import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+
+import { PagesComponent } from "./pages/pages.component";
+import { CreditsComponent } from "./credits/credits.component";
+import { MenuComponent } from "./menu/menu.component";
 import { AppComponent } from "./app.component";
-import { ResultsComponent } from "./results/results.component";
-import { HomeComponent } from "./home/home.component";
-import { SiteLayoutComponent } from "./layout/site-layout/site-layout.component";
-import { SiteHeaderComponent } from "./layout/site-header/site-header.component";
-import { SiteFooterComponent } from "./layout/site-footer/site-footer.component";
-import { RestService } from "./services/rest.service";
-import { DataService } from "./services/data.service";
-import { routing } from "./app.routing";
-import { GradesComponent } from "./grades/grades.component";
-import { PagesComponent } from './pages/pages.component';
-import { CreditsComponent } from './credits/credits.component';
-import { MenuComponent } from './menu/menu.component';
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    ResultsComponent,
-    SiteLayoutComponent,
-    SiteHeaderComponent,
-    SiteFooterComponent,
-    GradesComponent,
-    PagesComponent,
-    CreditsComponent,
-    MenuComponent,
-  ],
-  imports: [
-    BrowserModule,
-    HttpModule,
-    FormsModule,
-    routing,
-    NgbModule,
-    MatIconModule,
-    MatProgressSpinnerModule,
-  ],
-  providers: [RestService, DataService],
+  declarations: [AppComponent, PagesComponent, CreditsComponent, MenuComponent],
+  imports: [BrowserModule],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
